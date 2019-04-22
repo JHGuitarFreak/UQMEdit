263542b6387a5468aa5b78aff880e2a7120e9feb
Serosis <serosis@gmail.com>
2019-04-22 04:23:00 -0700

    Initial Project files

---
+++ UQMEdit/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UQMEdit
+{
+	public partial class Main : Form
+	{
+		public Main() {
+			InitializeComponent();
+		}
+	}
+}
