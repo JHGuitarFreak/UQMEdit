fbf37146bf5c5a886a7f086fff5de2813c541a08
Serosis <serosis@gmail.com>
2019-04-22 09:27:32 -0700

    Add in the title text switch and directory switch

--- UQMEdit/Main.cs
+++ UQMEdit/Main.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Globalization;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace UQMEdit
@@ -15,14 +14,46 @@ namespace UQMEdit
 		private string CurrentDir;
 		private string CurrentFile = "";
 		public static string FileName = "";
+		private object[] shipModules;
 
 		public Main() {
 			InitializeComponent();
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+			this.shipModules = Modules.CreateModules();
+
+			foreach (object current4 in this.ShipsBox.Controls) {
+				if (current4 is ComboBox) {
+					(current4 as ComboBox).Items.AddRange(Vars.escortNames);
+				}
+			}
+			this.CurrentStatus.Items.AddRange(Vars.statusName);
+			this.Spoilers.Checked = false;
+
+			string PathVanilla, PathHD, PathMegaMod, PathRemix, PathDesired;
+			string PathAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			PathVanilla = PathAppData + "\\uqm\\save";
+			PathHD = PathAppData + "\\uqmhd\\save";
+			PathRemix = PathAppData + "\\uqmhdremix\\save";
+			PathMegaMod = PathAppData + "\\UQM-MegaMod\\save";
+
+			if (Directory.Exists(PathVanilla)) {
+				PathDesired = PathVanilla;
+			} else if (Directory.Exists(PathHD)) {
+				PathDesired = PathHD;
+			} else if (Directory.Exists(PathRemix)) {
+				PathDesired = PathRemix;
+			} else if (Directory.Exists(PathMegaMod)) {
+				PathDesired = PathMegaMod;
+			} else {
+				PathDesired = Directory.GetCurrentDirectory();
+			}
+
+			CurrentDir = PathDesired;
+			this.StarList.Items.AddRange(ParseStars.LoadStars(false));
 		}
 
 		private void Open_Click(object sender, EventArgs e) {
 			System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
-			CurrentDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 			openFileDialog.Title = "Open Save File";
 			openFileDialog.Filter = "Save File (*.*)|*.*";
 			openFileDialog.InitialDirectory = CurrentDir;
@@ -30,22 +61,43 @@ namespace UQMEdit
 			if (openFileDialog.ShowDialog() == DialogResult.OK) {
 				CurrentFile = openFileDialog.FileName;
 				FileName = Path.GetFileName(CurrentFile);
-				this.Text = "The UQM Save Editor - " + FileName;
 				this.Reload.Enabled = true;
 				this.Save.Enabled = true;
 				this.Tabs.Enabled = true;
 
-				//Read.Open(CurrentFile, this);
+				Read.Open(CurrentFile, this);
+
+				string TitleText = "The UQM Save Editor";
+				this.SeedBox.Visible = false;
+				switch (Read.SaveVersion) {
+					case 3:
+						TitleText += ": macOS - ";
+						break;
+					case 2:
+						TitleText += ": MegaMod - ";
+						this.SeedBox.Visible = true;
+						break;
+					case 1:
+						TitleText += ": HD-mod - ";
+						break;
+					case 0:
+						TitleText += ": Vanilla - ";
+						break;
+					default:
+						break;
+				}
+				this.Text = TitleText + (Read.SaveVersion > 0 ? (Read.TimeDate + " - " + Read.SaveName) : Read.TimeDate);
+
 				CurrentDir = CurrentFile;
 			}
 		}
 
 		private void Reload_Click(object sender, EventArgs e) {
-			//Read.Open(CurrentFile, this);
+			Read.Open(CurrentFile, this);
 		}
 
 		private void Save_Click(object sender, EventArgs e) {
-			//Write.Save(CurrentFile, this);
+			Write.Save(CurrentFile, this);
 		}
 	}
 }
