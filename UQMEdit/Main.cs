6cbca4345c4f68ecf413a06ebd4829fe68c96fbe
Serosis <serosis@gmail.com>
2019-04-22 07:17:46 -0700

    Add the open file dialog

--- UQMEdit/Main.cs
+++ UQMEdit/Main.cs
@@ -12,8 +12,40 @@ namespace UQMEdit
 {
 	public partial class Main : Form
 	{
+		private string CurrentDir;
+		private string CurrentFile = "";
+		public static string FileName = "";
+
 		public Main() {
 			InitializeComponent();
+		}
+
+		private void Open_Click(object sender, EventArgs e) {
+			System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			CurrentDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			openFileDialog.Title = "Open Save File";
+			openFileDialog.Filter = "Save File (*.*)|*.*";
+			openFileDialog.InitialDirectory = CurrentDir;
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK) {
+				CurrentFile = openFileDialog.FileName;
+				FileName = Path.GetFileName(CurrentFile);
+				this.Text = "The UQM Save Editor - " + FileName;
+				this.Reload.Enabled = true;
+				this.Save.Enabled = true;
+				this.Tabs.Enabled = true;
+
+				//Read.Open(CurrentFile, this);
+				CurrentDir = CurrentFile;
+			}
+		}
+
+		private void Reload_Click(object sender, EventArgs e) {
+			//Read.Open(CurrentFile, this);
+		}
+
+		private void Save_Click(object sender, EventArgs e) {
+			//Write.Save(CurrentFile, this);
 		}
 	}
 }
