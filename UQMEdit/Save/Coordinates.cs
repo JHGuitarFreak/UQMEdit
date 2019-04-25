25e7bed7e0965f5f6781bc55311c020bfc13cd21
Serosis <serosis@gmail.com>
2019-04-25 14:49:33 -0700

    Finished up the current save code

--- UQMEdit/Save/Coordinates.cs
+++ UQMEdit/Save/Coordinates.cs
@@ -1,13 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows.Forms;
 
 namespace UQMEdit
 {
 	partial class Write
 	{
 		public static void Coordinates() {
+			int snum;
+
+			// UniverseX
+			decimal UniverseX = Window.UniverseX.Value * 10;
+			snum = Functions.UniverseToLogX(decimal.ToInt32(UniverseX));
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.LogX, Offs.MM.LogX), snum, 4, 159735);
+
+			// UniverseY
+			decimal UniverseY = Window.UniverseY.Value * 10;
+			snum = Functions.UniverseToLogY(decimal.ToInt32(UniverseY));
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.LogY, Offs.MM.LogY), snum, 4, 191990);
 		}
 	}
 }
--- UQMEdit/Save/Summary.cs
+++ UQMEdit/Save/Summary.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,72 @@ namespace UQMEdit
 	partial class Write
 	{
 		public static void Summary() {
+			// Resource Units
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.ResUnits, Offs.MM.ResUnits), Window.ResUnits.Value, 4, 0xFFFFFFFF, true);
+			// Fuel
+			decimal ShipFuel = Window.ShipFuel.Value * 100;
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Fuel, Offs.MM.Fuel), Decimal.ToUInt32(ShipFuel), 4, 161000, true);
+			// Crew
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.SiSCrew, Offs.MM.SiSCrew), Window.ShipCrew.Value, 2, 800, true);
+			// Total Minerals
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.TotalMinerals, Offs.MM.TotalMinerals), Window.TotalMinerals.Value, 2, 8000, true);
+			// Bio Data
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.BioData, Offs.MM.BioData), Window.BioData.Value, 2, 0xFFFF, true);
+
+			// Modules
+			byte Modules, i = 0;
+			foreach (object Module in Window.ModulesBox.Controls) {
+				if (Module is ComboBox) {
+					int Index = (Module as ComboBox).SelectedIndex;
+					if (Index > 0) {
+						Modules = (byte)(Index + 2);
+					} else {
+						Modules = 22;
+					}
+					Functions.WriteOffset(Functions.OffsPick(Offs.HD.ModuleSlots + i, Offs.MM.ModuleSlots + i), Modules, 1, 22, true);
+					i++;
+				}
+			}
+
+			// Thrusters
+			byte Thrusters, j = 0;
+			foreach (object Thruster in Window.ThrusterBox.Controls) {
+				if (Thruster is CheckBox) {
+					bool Index = (Thruster as CheckBox).Checked;
+					Thrusters = (byte)(Index ? 1 : 20);
+					Functions.WriteOffset(Functions.OffsPick(Offs.HD.DriveSlots[j], Offs.MM.DriveSlots[j]), Thrusters, 1, 20, true);
+					j++;
+				}
+			}
+
+			// Thrusters
+			byte Jets, k = 0;
+			foreach (object Jet in Window.JetsBox.Controls) {
+				if (Jet is CheckBox) {
+					bool Index = (Jet as CheckBox).Checked;
+					Jets = (byte)(Index ? 2 : 21);
+					Functions.WriteOffset(Functions.OffsPick(Offs.HD.JetSlots[k], Offs.MM.JetSlots[k]), Jets, 1, 21, true);
+					k++;
+				}
+			}
+
+			// Landers
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Landers, Offs.MM.Landers), Window.Landers.Value, 1, 10);
+
+			// Cargo
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Common, Offs.MM.Common), Window.Common.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Corrosive, Offs.MM.Corrosive), Window.Corrosive.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.BaseMetal, Offs.MM.BaseMetal), Window.BaseMetal.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.NobleGas, Offs.MM.NobleGas), Window.NobleGas.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.RareEarth, Offs.MM.RareEarth), Window.RareEarth.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Precious, Offs.MM.Precious), Window.Precious.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Radioactive, Offs.MM.Radioactive), Window.Radioactive.Value, 2, 0xFFFF);
+			Functions.WriteOffset(Functions.OffsPick(Offs.HD.Exotic, Offs.MM.Exotic), Window.Exotic.Value, 2, 0xFFFF);
+
+			// Ship Name
+			Functions.WriteOffsetString(Functions.OffsPick(Offs.HD.ShipName, Offs.MM.ShipName), Window.ShipName.Text, 16);
+			// Captain Name
+			Functions.WriteOffsetString(Functions.OffsPick(Offs.HD.CaptainName, Offs.MM.CaptainName), Window.CommanderName.Text, 16);
 		}
 	}
 }
